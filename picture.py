72a765cda230e8b15fd16940f152ac84665c595b
Olasubulumi Ogo-Oluwa Jesutomi <33330651+tomilashy@users.noreply.github.com>
2018-06-14 01:56:43 -0400

    done by Diogo Machado

--- picture.py
+++ picture.py
@@ -1,33 +1,25 @@
-'''
-Created on Jun 11, 2018
-
-@author: Jesutomi
-'''
-
-
-import time
-import pathlib
-import glob
 import os
+import exifread
+import pathlib
 import shutil
 
 
-files = glob.glob("*.jpg")
-files.sort(key=os.path.getctime)
-#print("\n".join(files))
-#printing out files in sorted form
-for  x in files:
-    times=os.path.getctime(x)
-    real=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(times))
-    print(f"{x}\t\t\t{real}")
 
-for  x in files:
-    times=os.path.getmtime(x)
-    folder=str(time.strftime("%B",time.localtime(times))) +" "+ str(time.strftime("%Y",time.localtime(times)))
-    #print(folder)
+path = os.getcwd()
 
-    if  os.path.exists(folder):
-        pass
-    else:
-        pathlib.Path(folder).mkdir()
-    shutil.move(x,folder)
+print(path)
+
+TTags = [('DateTimeOriginal', 'DateTime')]
+for file in os.listdir(path):
+   if file.endswith('.jpg'):
+       pic = open(file,'rb')
+       tags = exifread.process_file(pic)
+       for tag in tags.keys():
+           if tag == 'EXIF DateTimeOriginal':
+               print(tags[tag])
+               date = str(tags[tag])[0:4]
+               if os.path.exists(date):
+                   print('Folder already exists!')
+               else:
+                   pathlib.Path(date).mkdir()
+               shutil.move(file,date)
