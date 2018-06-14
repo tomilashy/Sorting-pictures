a846ad606c2e1c57f6365f6f7bb364816f99cc55
Olasubulumi Ogo-Oluwa Jesutomi <33330651+tomilashy@users.noreply.github.com>
2018-06-14 01:51:29 -0400

    code sorts and moves file to folders

--- picture.py
+++ picture.py
@@ -9,21 +9,25 @@ import time
 import pathlib
 import glob
 import os
-import  datetime
+import shutil
 
 
 files = glob.glob("*.jpg")
 files.sort(key=os.path.getctime)
-print("\n".join(files))
+#print("\n".join(files))
+#printing out files in sorted form
+for  x in files:
+    times=os.path.getctime(x)
+    real=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(times))
+    print(f"{x}\t\t\t{real}")
 
-times=os.path.getmtime("AlertImage_ContactLow.jpg")
+for  x in files:
+    times=os.path.getmtime(x)
+    folder=str(time.strftime("%B",time.localtime(times))) +" "+ str(time.strftime("%Y",time.localtime(times)))
+    #print(folder)
 
-
-real=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(times))
-
-folder=str(time.strftime("%B",time.localtime(times))) +" "+ str(time.strftime("%Y",time.localtime(times)))
-print(folder)
-
-
-pathlib.Path(folder).mkdir()
-os.path.exists(folder)
+    if  os.path.exists(folder):
+        pass
+    else:
+        pathlib.Path(folder).mkdir()
+    shutil.move(x,folder)
