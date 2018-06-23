3377e43e50b46318d27e29a1eb945e64627b3498
Olasubulumi Ogo-Oluwa Jesutomi <33330651+tomilashy@users.noreply.github.com>
2018-06-22 22:52:22 -0400

    writing into a new csv file the names of the pics

--- picture.py
+++ picture.py
@@ -4,11 +4,14 @@ Created on Jun 11, 2018
 @author: Jesutomi
 '''
 
+
 import time
 import pathlib
 import glob
 import os
 import shutil
+from csv import reader, writer,DictWriter
+    
 
 
 files = glob.glob("*.jpg")
@@ -22,13 +25,30 @@ for  x in files:
     real=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(times))
     print(f"{x}\t\t\t{real}")
 
-for  x in files:
-    times=os.path.getmtime(x)
-    folder=str(time.strftime("%B",time.localtime(times))) +" "+ str(time.strftime("%Y",time.localtime(times)))
-    #print(folder)
+num=0
+with open("Picture file.csv", "w") as file:
+    headers = ["S/N","Name", "Time", "New folder"]
+    csv_writer = DictWriter(file, fieldnames=headers,delimiter="|")
+    csv_writer.writeheader()
+    for  x in files:
+        times=os.path.getmtime(x)
+        folder=str(time.strftime("%B",time.localtime(times))) +" "+ str(time.strftime("%Y",time.localtime(times)))
+        #print(folder) 
+        
+        num+=1
+        if num:
+          
+            csv_writer.writerow({
+                "S/N":f"{num}",
+                "Name": f"{x}",
+                "Time": f"{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(times))}",
+                "New folder": f"{folder}"
+            })
+            
+        if  os.path.exists(folder):
+            pass
+        else:
+            pathlib.Path(folder).mkdir()
+        shutil.move(x,folder)
+    
 
-    if  os.path.exists(folder):
-        pass
-    else:
-        pathlib.Path(folder).mkdir()
-    shutil.move(x,folder)
